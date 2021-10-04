e10270e8ad6a1c6ee4532acfd6bcd5e44f268ae8
kenesparta <kenesparta@pm.me>
2021-10-04 03:20:21 -0500

    COMMIT #4: Implementing unit test
    Adding race condition to AssembleVehicle

--- assemblyspot/assemblyspot.go
+++ assemblyspot/assemblyspot.go
@@ -28,14 +28,14 @@ func (s *AssemblySpot) GetAssembledLogs() string {
 
 //hint: improve this function to execute this process concurrenlty
 func (s *AssemblySpot) AssembleVehicle() (*vehicle.Car, error) {
-
 	if s.vehicleToAssemble == nil {
 		return nil, errors.New("no vehicle set to start assembling")
 	}
 	s.assemblyLog = ""
 	var (
 		wg       sync.WaitGroup
-		pipeline = []func(*sync.WaitGroup){
+		mu       sync.Mutex
+		pipeline = []func(*sync.WaitGroup, *sync.Mutex){
 			s.assembleChassis,
 			s.assembleTires,
 			s.assembleEngine,
@@ -49,58 +49,72 @@ func (s *AssemblySpot) AssembleVehicle() (*vehicle.Car, error) {
 	// This executes every process on a different go-routine
 	for _, process := range pipeline {
 		wg.Add(1)
-		go process(&wg)
+		go process(&wg, &mu)
 	}
 
 	wg.Wait()
 	return s.vehicleToAssemble, nil
 }
 
-func (s *AssemblySpot) assembleChassis(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleChassis(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Chassis = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Chassis at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleTires(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleTires(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Tires = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Tires at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleEngine(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleEngine(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Engine = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Engine at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleElectronics(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleElectronics(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Electronics = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Electronics at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleDash(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleDash(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Dash = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Dash at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleSeats(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleSeats(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Sits = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Sits at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
 
-func (s *AssemblySpot) assembleWindows(wg *sync.WaitGroup) {
+func (s *AssemblySpot) assembleWindows(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
 	s.vehicleToAssemble.Windows = "Assembled"
 	time.Sleep(1 * time.Second)
+	mu.Lock()
 	s.assemblyLog += fmt.Sprintf("Windows at [%s], ", time.Now().Format("2006-01-02 15:04:05.000"))
+	mu.Unlock()
 }
--- factory/factory_test.go
+++ factory/factory_test.go
@@ -1,9 +1,11 @@
 package factory
 
 import (
-	"testing"
-
+	".main.go/assemblyspot"
+	".main.go/vehicle"
 	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
 )
 
 type factoryUnitTestSuite struct {
@@ -20,8 +22,87 @@ func TestFactoryUnitTestSuite(t *testing.T) {
 	suite.Run(t, &factoryUnitTestSuite{})
 }
 
-func (s *factoryUnitTestSuite) TestSamble() {
-	//code here
-	// Assert
-	s.Assert().Equal(1, 1)
+func (s *factoryUnitTestSuite) TestStartAssemblingProcessNotEmptyChannel() {
+	s.adapter = New()
+	tests := []struct {
+		name  string
+		input struct {
+			carsAmount        int
+			vehiclesAssembled chan vehicle.Car
+		}
+		output float64
+	}{
+		{
+			name: "First case",
+			input: struct {
+				carsAmount        int
+				vehiclesAssembled chan vehicle.Car
+			}{
+				carsAmount:        1,
+				vehiclesAssembled: make(chan vehicle.Car, 1),
+			},
+			output: 1.01,
+		},
+		{
+			name: "Zero case",
+			input: struct {
+				carsAmount        int
+				vehiclesAssembled chan vehicle.Car
+			}{
+				carsAmount:        0,
+				vehiclesAssembled: make(chan vehicle.Car, 0),
+			},
+			output: 0.01,
+		},
+		{
+			name: "Default case",
+			input: struct {
+				carsAmount        int
+				vehiclesAssembled chan vehicle.Car
+			}{
+				carsAmount:        9,
+				vehiclesAssembled: make(chan vehicle.Car, 9),
+			},
+			output: 2.01,
+		},
+	}
+
+	for _, tt := range tests {
+		start := time.Now()
+		s.adapter.StartAssemblingProcess(tt.input.carsAmount, tt.input.vehiclesAssembled)
+		for car := 0; car < tt.input.carsAmount; car++ {
+			v := <-tt.input.vehiclesAssembled
+			s.Assert().NotEqual("", v.Id)
+			s.Assert().NotEqual("", v.AssembleLog)
+		}
+		duration := time.Since(start)
+		s.Assert().Condition(func() (success bool) {
+			return duration.Seconds() <= tt.output
+		}, "")
+	}
+}
+
+func (s *factoryUnitTestSuite) TestAssembleVehicleLogsNotEmpty() {
+	var (
+		idleSpot    = assemblyspot.AssemblySpot{}
+		vehicleTest = vehicle.Car{
+			Id:            0,
+			Chassis:       "NotSet",
+			Tires:         "NotSet",
+			Engine:        "NotSet",
+			Electronics:   "NotSet",
+			Dash:          "NotSet",
+			Sits:          "NotSet",
+			Windows:       "NotSet",
+			EngineStarted: false,
+			TestingLog:    "",
+			AssembleLog:   "",
+		}
+	)
+	idleSpot.SetVehicle(&vehicleTest)
+	_, err := idleSpot.AssembleVehicle()
+	if err != nil {
+		s.Fail("Nil Object")
+	}
+	s.Assert().NotEqual("", idleSpot.GetAssembledLogs())
 }
