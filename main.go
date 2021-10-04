4f45e4206d89bebdfd487631950c0c5994363fd0
kenesparta <kenesparta@pm.me>
2021-10-04 03:34:56 -0500

    COMMIT #4: Implementing unit test
    - Adding race condition to AssembleVehicle
    - Change unit test name

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ func main() {
 	//each vehicle delivered to main should display testinglogs and assemblelogs with the respective vehicle id
 	factory.StartAssemblingProcess(carsAmount, vehiclesAssembled)
 
-	// Reading the channel of the vehiclesAssembled
+	// Reading the vehiclesAssembled channel
 	for car := 0; car < carsAmount; car++ {
 		v := <-vehiclesAssembled
 		fmt.Printf("TestingLogs: %s \n", v.TestingLog)
 		fmt.Printf("ID: %d => AssembledLogs: %s \n", v.Id, v.AssembleLog)
 		fmt.Println("-----------")
 	}
-	defer close(vehiclesAssembled)
+	close(vehiclesAssembled)
 }
